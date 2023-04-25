bf01b24971eac154e28e901c9069e00023ad2b3b
unknown <fifakawior@gmail.com>
2023-04-25 21:49:03 +0200

    First commit, begining of the project

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True) 
+    
