610b2fa821745a76ff429a0523f53518324d902b
unknown <fifakawior@gmail.com>
2023-04-26 19:24:54 +0200

    Second commit, add tasks to database and display it

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask import request, redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -15,10 +16,26 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    tasks_not_completed = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html', tasks_not_completed =tasks_not_completed)
+
+@app.route('/add-task', methods=['POST', 'GET'])
+def task_add():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return 'Task was added'
+        except:
+            return 'There was an issue adding the task'
+    else:
+        
+        return render_template('add-task.html')
 if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True) 
-    
+#C:\Users\PC\AppData\Local\Programs\Python\Python311\python.exe
